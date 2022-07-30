181c9f05198c38f96ec391194c74de44be703a6c
trwinowiecki <trw0511@yahoo.com>
2022-07-29 23:04:39 -0400

    add product list

---
+++ components/ProductItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <a>
+          <Image
+            src={product.image}
+            width={500}
+            height={500}
+            layout="responsive"
+            objectFit="cover"
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
