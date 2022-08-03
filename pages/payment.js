0f70577c1b6be370eac39140d30cbb5b5b398aae
trwinowiecki <trw0511@yahoo.com>
2022-08-03 00:29:31 -0400

    add PaymentPage

---
+++ pages/payment.js
@@ -0,0 +1,78 @@
+import React, { useContext, useEffect, useState } from 'react';
+import CheckoutWizard from '../components/CheckoutWizard';
+import { useRouter } from 'next/router';
+import toast from 'react-toastify';
+import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
+
+export default function PaymentPage() {
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress, paymentMethod } = cart;
+
+  const router = useRouter();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!selectedPaymentMethod) {
+      return toast.error('Payment method is required');
+    }
+    dispatch({ type: 'SAVE_PAYMENT_METHOD', payload: selectedPaymentMethod });
+    Cookies.set(
+      'cart',
+      JSON.stringify({
+        ...cart,
+        paymentMethod: selectedPaymentMethod,
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      router.push('/shipping');
+    }
+    setSelectedPaymentMethod(paymentMethod || '');
+  }, [paymentMethod, router, shippingAddress.address]);
+
+  return (
+    <Layout title="Payment">
+      <CheckoutWizard activeStep={2} />
+      <form className="mx-auto max-w-screen-md" onSubmit={submitHandler}>
+        <h1 className="mb-4 text-xl">Payment Method</h1>
+        {['PayPal', 'Stripe'].map((payment) => (
+          <div key={payment} className="mb-4">
+            <input
+              name="paymentMethod"
+              className="p-2 outline-none focus:ring-0"
+              id={payment}
+              type="radio"
+              checked={selectedPaymentMethod === payment}
+              onChange={() => setSelectedPaymentMethod(payment)}
+            />
+            <label className="p-2" htmlFor={payment}>
+              {payment}
+            </label>
+          </div>
+        ))}
+        <div className="mb-4 flex justify-between">
+          <button
+            onClick={() => router.push('/shipping')}
+            type="button"
+            className="default-button"
+          >
+            Back
+          </button>
+          <button
+            onClick={() => router.push('/placeorder')}
+            className="primary-button"
+          >
+            Next
+          </button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
