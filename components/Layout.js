df7d26714d81f36722980be743eda24729413f4a
trwinowiecki <trw0511@yahoo.com>
2022-08-02 22:28:37 -0400

    implement login API with NextAuth

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Link from 'next/Link';
 import Head from 'next/head';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react';
 import { Store } from '../utils/Store';
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
   const { state } = useContext(Store);
   const cart = state.cart;
   const [cartItemsCount, setcartItemsCount] = useState(0);
@@ -18,6 +23,8 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Ethereal Sun Designs Jewelry" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
 
       <div className="flex min-h-screen flex-col justify-between">
         <header>
@@ -36,9 +43,15 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
-              </Link>
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
